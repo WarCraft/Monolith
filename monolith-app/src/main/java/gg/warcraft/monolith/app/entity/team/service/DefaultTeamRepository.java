51b1db5143a87943c38237439ed95299a10edccf
BuildTools <unconfigured@null.spigotmc.org>
2019-03-31 20:24:30 +0200

    Remove unused persistence functionality

--- monolith-app/src/main/java/gg/warcraft/monolith/app/entity/team/service/DefaultTeamRepository.java
+++ monolith-app/src/main/java/gg/warcraft/monolith/app/entity/team/service/DefaultTeamRepository.java
@@ -2,7 +2,6 @@ package gg.warcraft.monolith.app.entity.team.service;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import gg.warcraft.monolith.api.core.PersistenceService;
 import gg.warcraft.monolith.api.entity.team.Team;
 import gg.warcraft.monolith.api.entity.team.service.TeamRepository;
 
@@ -10,35 +9,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Singleton
 public class DefaultTeamRepository implements TeamRepository {
-    private static final String ENTITY_TEAM_KEY_PREFIX = "entity:team:";
-
-    private final PersistenceService persistenceService;
     private final Map<String, Team> teams;
 
     @Inject
-    public DefaultTeamRepository(PersistenceService persistenceService) {
-        this.persistenceService = persistenceService;
+    public DefaultTeamRepository() {
         this.teams = new HashMap<>();
-    }
-
-    String createTeamKey(UUID entityId) {
-        return ENTITY_TEAM_KEY_PREFIX + entityId;
     }
 
     @Override
     public Team getTeam(String name) {
         return teams.get(name);
-    }
-
-    @Override
-    public Team getTeam(UUID entityId) {
-        String teamKey = createTeamKey(entityId);
-        String team = persistenceService.get(teamKey);
-        return teams.get(team);
     }
 
     @Override
@@ -49,18 +32,5 @@ public class DefaultTeamRepository implements TeamRepository {
     @Override
     public void save(Team team) {
         teams.put(team.getName(), team);
-    }
-
-    @Override
-    public void save(Team team, UUID entityId) {
-        String teamKey = createTeamKey(entityId);
-        String teamName = team.getName();
-        persistenceService.set(teamKey, teamName);
-    }
-
-    @Override
-    public void delete(UUID entityId) {
-        String teamKey = createTeamKey(entityId);
-        persistenceService.delete(teamKey);
     }
 }
