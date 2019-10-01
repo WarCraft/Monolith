3b8620b0fa34a8fc664636fbcc8ad3d9ae4fd885
fishb6nes <fishb6nes@users.noreply.github.com>
2019-10-01 10:54:06 +0200

    Code cleanup

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/BlockLocation.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/world/BlockLocation.scala
@@ -3,38 +3,51 @@ package gg.warcraft.monolith.api.world
 import gg.warcraft.monolith.api.math.Vector3i
 
 case class BlockLocation(
-  world: World,
-  translation: Vector3i
+    world: World,
+    translation: Vector3i
 ) {
   val x: Int = translation.x
   val y: Int = translation.y
   val z: Int = translation.z
 
-  def this(world: World, x: Int, y: Int, z: Int) {
+  def this(world: World, x: Int, y: Int, z: Int) =
     this(world, Vector3i(x, y, z))
-  }
 
   def add(x: Int, y: Int, z: Int): BlockLocation =
     copy(translation = translation.add(x, y, z))
 
-  def add(vec: Vector3i): BlockLocation = add(vec.x, vec.y, vec.z)
+  def add(vec: Vector3i): BlockLocation =
+    add(vec.x, vec.y, vec.z)
 
-  def add(loc: BlockLocation): BlockLocation = add(loc.x, loc.y, loc.z)
+  def add(loc: BlockLocation): BlockLocation =
+    add(loc.x, loc.y, loc.z)
 
   def subtract(x: Int, y: Int, z: Int): BlockLocation =
     copy(translation = translation.subtract(x, y, z))
 
-  def subtract(vec: Vector3i): BlockLocation = subtract(vec.x, vec.y, vec.z)
+  def subtract(vec: Vector3i): BlockLocation =
+    subtract(vec.x, vec.y, vec.z)
 
-  def subtract(loc: BlockLocation): BlockLocation = subtract(loc.x, loc.y, loc.z)
+  def subtract(loc: BlockLocation): BlockLocation =
+    subtract(loc.x, loc.y, loc.z)
 
-  def toLocation: Location = Location(world, translation.toVector3f)
+  def toLocation: Location =
+    Location(world, translation.toVector3f)
 
   /* Java interop */
 
-  def withWorld(world: World): BlockLocation = copy(world = world)
-  def withTranslation(translation: Vector3i): BlockLocation = copy(translation = translation)
-  def withX(x: Int): BlockLocation = copy(translation = translation.withX(x))
-  def withY(y: Int): BlockLocation = copy(translation = translation.withY(y))
-  def withZ(z: Int): BlockLocation = copy(translation = translation.withZ(z))
+  def withWorld(world: World): BlockLocation =
+    copy(world = world)
+
+  def withTranslation(translation: Vector3i): BlockLocation =
+    copy(translation = translation)
+
+  def withX(x: Int): BlockLocation =
+    copy(translation = translation.withX(x))
+
+  def withY(y: Int): BlockLocation =
+    copy(translation = translation.withY(y))
+
+  def withZ(z: Int): BlockLocation =
+    copy(translation = translation.withZ(z))
 }
--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/Location.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/world/Location.scala
@@ -1,55 +1,77 @@
 package gg.warcraft.monolith.api.world
 
-import gg.warcraft.monolith.api.math.{ Vector3f, Vector3i }
+import gg.warcraft.monolith.api.math.{Vector3f, Vector3i}
 
 case class Location(
-  world: World,
-  translation: Vector3f,
-  rotation: Vector3f = Vector3f.ZERO_PITCH_YAW
+    world: World,
+    translation: Vector3f,
+    rotation: Vector3f = Vector3f.ZERO_PITCH_YAW
 ) {
   val x: Float = translation.x
   val y: Float = translation.y
   val z: Float = translation.z
 
   private lazy val pitchYaw: (Float, Float) = rotation.toPitchYaw
+
   lazy val pitch: Float = pitchYaw._1
   lazy val yaw: Float = pitchYaw._2
 
-  def this(world: World, x: Float, y: Float, z: Float) {
+  def this(world: World, x: Float, y: Float, z: Float) =
     this(world, Vector3f(x, y, z), Vector3f.ZERO_PITCH_YAW)
-  }
 
   def add(x: Float, y: Float, z: Float): Location =
     copy(translation = translation.add(x, y, z))
 
-  def add(vec: Vector3f): Location = add(vec.x, vec.y, vec.z)
+  def add(vec: Vector3f): Location =
+    add(vec.x, vec.y, vec.z)
 
-  def add(loc: Location): Location = add(loc.x, loc.y, loc.z)
+  def add(loc: Location): Location =
+    add(loc.x, loc.y, loc.z)
 
   def subtract(x: Float, y: Float, z: Float): Location =
     copy(translation = translation.subtract(x, y, z))
 
-  def subtract(vec: Vector3f): Location = subtract(vec.x, vec.y, vec.z)
+  def subtract(vec: Vector3f): Location =
+    subtract(vec.x, vec.y, vec.z)
 
-  def subtract(loc: Location): Location = subtract(loc.x, loc.y, loc.z)
+  def subtract(loc: Location): Location =
+    subtract(loc.x, loc.y, loc.z)
 
-  def distanceTo(target: Location): Float = translation.distanceTo(target.translation)
+  def distanceTo(target: Location): Float =
+    translation.distanceTo(target.translation)
 
-  def toBlockLocation: BlockLocation = BlockLocation(world,
-    Vector3i(Math.floor(x).toInt, Math.floor(y).toInt, Math.floor(z).toInt))
+  def toBlockLocation: BlockLocation = {
+    val floor = (x: Float) => Math.floor(x).toInt
+    val translation = Vector3i(floor(x), floor(y), floor(z))
+    BlockLocation(world, translation)
+  }
 
   /* Java interop */
 
-  def this(world: World, translation: Vector3f) {
+  def this(world: World, translation: Vector3f) =
     this(world, translation, Vector3f.ZERO_PITCH_YAW)
-  }
 
-  def withWorld(world: World): Location = copy(world = world)
-  def withTranslation(translation: Vector3f): Location = copy(translation = translation)
-  def withRotation(rotation: Vector3f): Location = copy(rotation = rotation)
-  def withX(x: Float): Location = copy(translation = translation.withX(x))
-  def withY(y: Float): Location = copy(translation = translation.withY(y))
-  def withZ(z: Float): Location = copy(translation = translation.withZ(z))
-  def withPitch(pitch: Float): Location = copy(rotation = Vector3f.apply(pitch, yaw))
-  def withYaw(yaw: Float): Location = copy(rotation = Vector3f.apply(pitch, yaw))
+  def withWorld(world: World): Location =
+    copy(world = world)
+
+  def withTranslation(translation: Vector3f): Location =
+    copy(translation = translation)
+
+  def withRotation(rotation: Vector3f): Location =
+    copy(rotation = rotation)
+
+  def withX(x: Float): Location =
+    copy(translation = translation.withX(x))
+
+  def withY(y: Float): Location =
+    copy(translation = translation.withY(y))
+
+  def withZ(z: Float): Location =
+    copy(translation = translation.withZ(z))
+
+  def withPitch(pitch: Float): Location =
+    copy(rotation = Vector3f.apply(pitch, yaw))
+
+  def withYaw(yaw: Float): Location =
+    copy(rotation = Vector3f.apply(pitch, yaw))
 }
