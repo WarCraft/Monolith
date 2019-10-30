645b6b14bfa12dd4fa2e3380cf9e5b9d2d6c5f63
fishb6nes <fishb6nes@users.noreply.github.com>
2019-10-30 22:47:00 +0100

    Update math param lists

--- monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/world/item/SpigotItemColorMapper.scala
+++ monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/world/item/SpigotItemColorMapper.scala
@@ -14,29 +14,29 @@ class SpigotItemColorMapper @Inject()(
     blockMapper.map(material)
 
   def map(item: ColoredItem): Material = item match {
-    case Banner(color, _)           => blockMapper.mapBanner(color)
-    case Bed(color, _)              => blockMapper.mapBed(color)
-    case Carpet(color, _)           => blockMapper.mapCarpet(color)
-    case Concrete(color, _)         => blockMapper.mapConcrete(color)
-    case ConcretePowder(color, _)   => blockMapper.mapConcretePowder(color)
-    case GlazedTerracotta(color, _) => blockMapper.mapGlazedTerracotta(color)
-    case Wool(color, _)             => blockMapper.mapWool(color)
+    case Banner(color, _, _, _, _, _)           => blockMapper.mapBanner(color)
+    case Bed(color, _, _, _, _, _)              => blockMapper.mapBed(color)
+    case Carpet(color, _, _, _, _, _)           => blockMapper.mapCarpet(color)
+    case Concrete(color, _, _, _, _, _)         => blockMapper.mapConcrete(color)
+    case ConcretePowder(color, _, _, _, _, _)   => blockMapper.mapConcretePowder(color)
+    case GlazedTerracotta(color, _, _, _, _, _) => blockMapper.mapGlazedTerracotta(color)
+    case Wool(color, _, _, _, _, _)             => blockMapper.mapWool(color)
 
     case _ => throw new IllegalArgumentException(s"$item")
   }
 
   def map(item: ColorableItem): Material = item match {
-    case Glass(None, _)        => Material.GLASS
-    case Glass(Some(color), _) => blockMapper.mapStainedGlass(color)
+    case Glass(None, _, _, _, _, _)        => Material.GLASS
+    case Glass(Some(color), _, _, _, _, _) => blockMapper.mapStainedGlass(color)
 
-    case GlassPane(None, _)        => Material.GLASS_PANE
-    case GlassPane(Some(color), _) => blockMapper.mapStainedGlassPane(color)
+    case GlassPane(None, _, _, _, _, _)        => Material.GLASS_PANE
+    case GlassPane(Some(color), _, _, _, _, _) => blockMapper.mapStainedGlassPane(color)
 
-    case ShulkerBox(None, _)        => Material.SHULKER_BOX
-    case ShulkerBox(Some(color), _) => blockMapper.mapShulkerBox(color)
+    case ShulkerBox(None, _, _, _, _, _)        => Material.SHULKER_BOX
+    case ShulkerBox(Some(color), _, _, _, _, _) => blockMapper.mapShulkerBox(color)
 
-    case Terracotta(None, _)        => Material.TERRACOTTA
-    case Terracotta(Some(color), _) => blockMapper.mapTerracotta(color)
+    case Terracotta(None, _, _, _, _, _)        => Material.TERRACOTTA
+    case Terracotta(Some(color), _, _, _, _, _) => blockMapper.mapTerracotta(color)
 
     case _ => throw new IllegalArgumentException(s"$item")
   }
