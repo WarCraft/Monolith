19c75ae0f3774218e7946dd297c4570948e11c60
fishb6nes <fishb6nes@users.noreply.github.com>
2019-10-31 10:27:16 +0100

    Map armor and tool materials

--- monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/world/item/SpigotItemMaterialMapper.scala
+++ monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/world/item/SpigotItemMaterialMapper.scala
@@ -2,7 +2,9 @@ package gg.warcraft.monolith.spigot.world.item
 
 import java.util
 
+import gg.warcraft.monolith.api.core.Extensions._
 import gg.warcraft.monolith.api.world.item.ItemMaterial
+import gg.warcraft.monolith.api.world.item.material.{ ArmorMaterial, ToolMaterial }
 import gg.warcraft.monolith.spigot.world.block.SpigotBlockMaterialMapper
 import javax.inject.Inject
 import org.bukkit.Material
@@ -15,73 +17,47 @@ class SpigotItemMaterialMapper @Inject()(
   private val cache =
     new util.EnumMap[Material, ItemMaterial](classOf[Material])
 
-  def map(material: Material): ItemMaterial =
-    cache.computeIfAbsent(material, compute)
+  def map(material: Material): ItemMaterial = cache.computeIfAbsent(material, {
+    case _ if !material.isItem => throw new IllegalArgumentException(s"$material")
+    case _ if material.isBlock => blockMapper.map(material).asInstanceOf[ItemMaterial]
 
-  private def compute(material: Material): ItemMaterial = material match {
-    case it if !it.isItem => throw new IllegalArgumentException(s"$material")
-    case it if it.isBlock => blockMapper.map(it).asInstanceOf[ItemMaterial]
+    case Material.DIAMOND_BOOTS | Material.DIAMOND_CHESTPLATE |
+         Material.DIAMOND_HELMET | Material.DIAMOND_HORSE_ARMOR |
+         Material.DIAMOND_LEGGINGS =>
+      ArmorMaterial.DIAMOND
 
-    case Material.CHAINMAIL_BOOTS => null
-    case Material.CHAINMAIL_CHESTPLATE => null
-    case Material.CHAINMAIL_HELMET => null
-    case Material.CHAINMAIL_LEGGINGS => null
+    case Material.DIAMOND_AXE | Material.DIAMOND_HOE |
+         Material.DIAMOND_PICKAXE | Material.DIAMOND_SHOVEL |
+         Material.DIAMOND_SWORD =>
+      ToolMaterial.DIAMOND
 
-    case Material.DIAMOND_AXE => null
-    case Material.DIAMOND_BOOTS => null
-    case Material.DIAMOND_CHESTPLATE => null
-    case Material.DIAMOND_HELMET => null
-    case Material.DIAMOND_HOE => null
-    case Material.DIAMOND_HORSE_ARMOR => null
-    case Material.DIAMOND_LEGGINGS => null
-    case Material.DIAMOND_PICKAXE => null
-    case Material.DIAMOND_SHOVEL => null
-    case Material.DIAMOND_SWORD => null
+    case Material.GOLDEN_BOOTS | Material.GOLDEN_CHESTPLATE |
+         Material.GOLDEN_HELMET | Material.GOLDEN_HORSE_ARMOR |
+         Material.GOLDEN_LEGGINGS =>
+      ArmorMaterial.GOLD
 
-    case Material.GOLDEN_APPLE => null
-    case Material.GOLDEN_AXE => null
-    case Material.GOLDEN_BOOTS => null
-    case Material.GOLDEN_CARROT => null
-    case Material.GOLDEN_CHESTPLATE => null
-    case Material.GOLDEN_HELMET => null
-    case Material.GOLDEN_HOE => null
-    case Material.GOLDEN_HORSE_ARMOR => null
-    case Material.GOLDEN_LEGGINGS => null
-    case Material.GOLDEN_PICKAXE => null
-    case Material.GOLDEN_SHOVEL => null
-    case Material.GOLDEN_SWORD => null
+    case Material.GOLDEN_AXE | Material.GOLDEN_HOE |
+         Material.GOLDEN_PICKAXE | Material.GOLDEN_SHOVEL |
+         Material.GOLDEN_SWORD =>
+      ToolMaterial.GOLD
 
-    case Material.IRON_AXE => null
-    case Material.IRON_BOOTS => null
-    case Material.IRON_CHESTPLATE => null
-    case Material.IRON_HELMET => null
-    case Material.IRON_HOE => null
-    case Material.IRON_HORSE_ARMOR => null
-    case Material.IRON_INGOT => null
-    case Material.IRON_LEGGINGS => null
-    case Material.IRON_NUGGET => null
-    case Material.IRON_PICKAXE => null
-    case Material.IRON_SHOVEL => null
-    case Material.IRON_SWORD => null
+    case Material.IRON_BOOTS | Material.IRON_CHESTPLATE |
+         Material.IRON_HELMET | Material.IRON_HORSE_ARMOR |
+         Material.IRON_LEGGINGS =>
+      ArmorMaterial.IRON
 
-    case Material.LEATHER_BOOTS => null
-    case Material.LEATHER_CHESTPLATE => null
-    case Material.LEATHER_HELMET => null
-    case Material.LEATHER_HORSE_ARMOR => null
-    case Material.LEATHER_LEGGINGS => null
+    case Material.IRON_AXE | Material.IRON_HOE |
+         Material.IRON_PICKAXE | Material.IRON_SHOVEL |
+         Material.IRON_SWORD =>
+      ToolMaterial.IRON
 
-    case Material.STONE_AXE => null
-    case Material.STONE_HOE => null
-    case Material.STONE_PICKAXE => null
-    case Material.STONE_SHOVEL => null
-    case Material.STONE_SWORD => null
+    case _ => material.name match {
+      case r"CHAINMAIL.*" => ArmorMaterial.CHAINMAIL
+      case r"LEATHER.*"   => ArmorMaterial.LEATHER
+      case r"STONE.*"     => ToolMaterial.STONE
+      case r"WOODEN.*"    => ToolMaterial.WOOD
 
-    case Material.WOODEN_AXE => null
-    case Material.WOODEN_HOE => null
-    case Material.WOODEN_PICKAXE => null
-    case Material.WOODEN_SHOVEL => null
-    case Material.WOODEN_SWORD => null
-
-    case _ => throw new IllegalArgumentException(s"$material")
-  }
+      case _ => throw new IllegalArgumentException(s"$material")
+    }
+  })
 }
