65dbfbe83cbbbf5097d237e7d57f07e6b510246b
fishb6nes <fishb6nes@users.noreply.github.com>
2019-10-31 12:10:42 +0100

    Add missing enchantments and fix max levels

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/enchantments.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/enchantments.scala
@@ -2,33 +2,33 @@ package gg.warcraft.monolith.api.world.item
 
 final case class AquaInfinity(level: Int)
     extends HelmetEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 }
 
 final case class BaneOfArthropods(level: Int)
     extends SwordEnchantment
     with AxeEnchantment {
-  val maxLevel = 3
+  val maxLevel = 5
 }
 
 final case class BlastProtection(level: Int)
     extends ArmorEnchantment {
-  val maxLevel = 3
+  val maxLevel = 4
 }
 
 final case class Channeling(level: Int)
     extends TridentEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 }
 
 final case class CurseOfBinding(level: Int)
     extends ArmorEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 } // TODO pumpkin / mob head / elytra
 
 final case class CurseOfVanishing(level: Int)
     extends ItemEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 }
 
 final case class DepthStrider(level: Int)
@@ -41,27 +41,27 @@ final case class Efficiency(level: Int)
     with PickaxeEnchantment
     with ShearsEnchantment
     with ShovelEnchantment {
-  val maxLevel = 3
+  val maxLevel = 5
 }
 
 final case class FeatherFalling(level: Int)
     extends BootsEnchantment {
-  val maxLevel = 3
+  val maxLevel = 4
 }
 
 final case class FireAspect(level: Int)
     extends SwordEnchantment {
-  val maxLevel = 3
+  val maxLevel = 2
 }
 
 final case class FireProtection(level: Int)
     extends ArmorEnchantment {
-  val maxLevel = 3
+  val maxLevel = 4
 }
 
 final case class Flame(level: Int)
     extends BowEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 }
 
 final case class Fortune(level: Int)
@@ -73,10 +73,30 @@ final case class Fortune(level: Int)
 
 final case class FrostWalker(level: Int)
     extends BootsEnchantment {
-  val maxLevel = 3
+  val maxLevel = 2
 }
 
 final case class Impaling(level: Int)
+    extends TridentEnchantment {
+  val maxLevel = 5
+}
+
+final case class Infinity(level: Int)
+    extends BowEnchantment {
+  val maxLevel = 1
+}
+
+final case class Knockback(level: Int)
+    extends SwordEnchantment {
+  val maxLevel = 2
+}
+
+final case class Looting(level: Int)
+    extends SwordEnchantment {
+  val maxLevel = 3
+}
+
+final case class Loyalty(level: Int)
     extends TridentEnchantment {
   val maxLevel = 3
 }
@@ -93,37 +113,37 @@ final case class Lure(level: Int)
 
 final case class Mending(level: Int)
     extends ItemEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 }
 
 final case class Multishot(level: Int)
     extends CrossbowEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 }
 
 final case class Piercing(level: Int)
     extends CrossbowEnchantment {
-  val maxLevel = 3
+  val maxLevel = 4
 }
 
 final case class Power(level: Int)
     extends BowEnchantment {
-  val maxLevel = 3
+  val maxLevel = 5
 }
 
 final case class ProjectileProtection(level: Int)
     extends ArmorEnchantment {
-  val maxLevel = 3
+  val maxLevel = 4
 }
 
 final case class Protection(level: Int)
     extends ArmorEnchantment {
-  val maxLevel = 3
+  val maxLevel = 4
 }
 
 final case class Punch(level: Int)
     extends BowEnchantment {
-  val maxLevel = 3
+  val maxLevel = 2
 }
 
 final case class QuickCharge(level: Int)
@@ -144,20 +164,20 @@ final case class Riptide(level: Int)
 final case class Sharpness(level: Int)
     extends SwordEnchantment
     with AxeEnchantment {
-  val maxLevel = 3
+  val maxLevel = 5
 }
 
 final case class SilkTouch(level: Int)
     extends AxeEnchantment
     with PickaxeEnchantment
     with ShovelEnchantment {
-  val maxLevel = 3
+  val maxLevel = 1
 }
 
 final case class Smite(level: Int)
     extends SwordEnchantment
     with AxeEnchantment {
-  val maxLevel = 3
+  val maxLevel = 5
 }
 
 final case class SweepingEdge(level: Int)
