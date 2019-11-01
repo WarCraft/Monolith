c7360c41eb927c1810d427306b1125fdf9c5ba9f
fishb6nes <fishb6nes@users.noreply.github.com>
2019-11-01 11:38:34 +0100

    Delegate mapping to type and variant mappers

--- monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/world/item/SpigotItemFactory.scala
+++ monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/world/item/SpigotItemFactory.scala
@@ -1,11 +1,5 @@
 package gg.warcraft.monolith.spigot.world.item
 
-import gg.warcraft.monolith.api.world.block.variant.{
-  SlabVariant,
-  StairsVariant,
-  StoniteVariant,
-  WallVariant
-}
 import gg.warcraft.monolith.api.world.item.{
   Item,
   ItemFactory,
@@ -14,25 +8,21 @@ import gg.warcraft.monolith.api.world.item.{
   VariableItem
 }
 import javax.inject.Inject
-import org.bukkit.Material
 
 class SpigotItemFactory @Inject() (
     itemMapper: SpigotItemMapper,
-    itemTypeMapper: SpigotItemTypeMapper,
+    typeMapper: SpigotItemTypeMapper,
     variantMapper: SpigotItemVariantMapper
 ) extends ItemFactory {
   override def create(`type`: ItemType): Item = {
-    val material = itemTypeMapper.map(`type`)
+    val material = typeMapper.map(`type`)
     val item = new SpigotItemStack(material)
     itemMapper.map(item).get
   }
 
-  override def create[T <: ItemVariant](variant: T): VariableItem[T] = itemOf({
-    case _: SlabVariant    => itemOf(Material.OAK_SLAB)
-    case _: StairsVariant  => itemOf(Material.OAK_STAIRS)
-    case _: StoniteVariant => itemOf(Material.ANDESITE)
-    case _: WallVariant    => itemOf(Material.COBBLESTONE_WALL)
-
-    case it: ItemVariant => variantMapper.map(it)
-  }).asInstanceOf[VariableItem[T]]
+  override def create[T <: ItemVariant](variant: T): VariableItem[T] = {
+    val material = variantMapper.map(variant)
+    val item = new SpigotItemStack(material)
+    itemMapper.map(item).get.asInstanceOf[VariableItem[T]]
+  }
 }
