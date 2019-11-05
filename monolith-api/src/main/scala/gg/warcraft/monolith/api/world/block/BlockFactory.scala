ee20fa79319e44d8553194df508f0a43d508cc49
fishb6nes <fishb6nes@users.noreply.github.com>
2019-11-05 16:54:03 +0100

    Remove block factory stub

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/block/BlockFactory.scala
+++
@@ -1,7 +0,0 @@
-package gg.warcraft.monolith.api.world.block
-
-trait BlockFactory {
-  def create(`type`: BlockType): Block
-  def create[T <: BlockVariant](variant: T): VariableBlock[T]
-  def create(`type`: String): Block
-}
