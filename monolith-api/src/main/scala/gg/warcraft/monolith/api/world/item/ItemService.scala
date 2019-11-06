d7ad7e656d9d0f3664ed07a47cda24655dcbc166
fishb6nes <fishb6nes@users.noreply.github.com>
2019-11-06 12:38:08 +0100

    Code cleanup

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/ItemService.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/ItemService.scala
@@ -7,7 +7,7 @@ import scala.annotation.varargs
 trait ItemService {
   def create(`type`: ItemType): Item
   def create[T <: ItemVariant](variant: T): VariableItem[T]
-  def create(`type`: String): Item
+  def create(data: String): Item
 
   def giveTo(playerId: UUID, `type`: ItemType, count: Int): Boolean
   def giveTo(playerId: UUID, variant: ItemVariant, count: Int): Boolean
