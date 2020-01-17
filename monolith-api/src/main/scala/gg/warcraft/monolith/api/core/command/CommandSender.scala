bf3e4bdcf2f5a83ee217c26d1d6f09aa5919924d
fishb6nes <fishb6nes@users.noreply.github.com>
2020-01-17 16:27:52 +0100

    Fix default boolean checks initializing too fast

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/core/command/CommandSender.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/core/command/CommandSender.scala
@@ -5,6 +5,7 @@ import java.util.UUID
 trait CommandSender {
   val name: String
   val playerId: Option[UUID]
-  val isPlayer: Boolean = playerId.isDefined
-  val isConsole: Boolean = playerId.isEmpty
+
+  def isPlayer: Boolean = playerId.isDefined
+  def isConsole: Boolean = playerId.isEmpty
 }
--- monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/core/command/SpigotCommandService.scala
+++ monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/core/command/SpigotCommandService.scala
@@ -8,14 +8,13 @@ import org.bukkit.Server
 class SpigotCommandService(
     implicit server: Server
 ) extends CommandService {
-  private val dispatchCommand = server.dispatchCommand _
   private val consoleSender = server.getConsoleSender
 
   override def dispatchCommand(playerId: UUID, cmd: String, args: String*): Unit = {
     val player = server.getPlayer(playerId)
-    if (player != null) dispatchCommand(player, s"${cmd} ${args.mkString(" ")}")
+    if (player != null) player.performCommand(s"$cmd ${args.mkString(" ")}")
   }
 
   override def dispatchConsoleCommand(cmd: String, args: String*): Unit =
-    dispatchCommand(consoleSender, s"${cmd} ${args.mkString(" ")}")
+    server.dispatchCommand(consoleSender, s"${cmd} ${args.mkString(" ")}")
 }
