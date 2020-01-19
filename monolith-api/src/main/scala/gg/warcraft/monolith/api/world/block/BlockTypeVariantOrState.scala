66e500b97e67b583539cc01ab0ce85d16618cfec
fishb6nes <fishb6nes@users.noreply.github.com>
2020-01-19 14:58:09 +0100

    Allow for deserialization from String to Block and Item data types

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/block/BlockTypeVariantOrState.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/world/block/BlockTypeVariantOrState.scala
@@ -1,3 +1,14 @@
 package gg.warcraft.monolith.api.world.block
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import gg.warcraft.monolith.api.Implicits
+
+object BlockTypeVariantOrState {
+  private val worldService = Implicits.worldService
+
+  @JsonCreator
+  def apply(data: String): BlockTypeVariantOrState =
+    worldService.parseData(data)
+}
+
 trait BlockTypeVariantOrState
--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/ItemTypeOrVariant.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/ItemTypeOrVariant.scala
@@ -1,3 +1,14 @@
 package gg.warcraft.monolith.api.world.item
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import gg.warcraft.monolith.api.Implicits
+
+object ItemTypeOrVariant {
+  private val itemService = Implicits.itemService
+
+  @JsonCreator
+  def apply(data: String): ItemTypeOrVariant =
+    itemService.parseData(data)
+}
+
 trait ItemTypeOrVariant
