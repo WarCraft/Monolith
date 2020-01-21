9981e2dd09b8ab9c567f80194254892794d71844
fishb6nes <fishb6nes@users.noreply.github.com>
2020-01-21 23:49:56 +0100

    Further flesh out command events

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/core/command/events.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/core/command/events.scala
@@ -1,18 +1,22 @@
 package gg.warcraft.monolith.api.core.command
 
+import gg.warcraft.monolith.api.core.event.{CancellableEvent, Event}
+
 trait CommandEvent
 
 // TODO fire these events
-case class CommandPreExecutedEvent(
+case class CommandPreExecuteEvent(
     sender: CommandSender,
     cmd: String,
     label: String,
-    args: String*
-)
+    args: Array[String],
+    cancelled: Boolean = false,
+    explicitlyAllowed: Boolean = false
+) extends CancellableEvent
 
-case class CommandExecutedEvent(
+case class CommandExecuteEvent(
     sender: CommandSender,
     cmd: String,
     label: String,
-    args: String*
-)
+    args: Array[String]
+) extends Event
