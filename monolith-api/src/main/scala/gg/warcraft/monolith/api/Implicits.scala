60b8dbe5905eeddaf780b852fd3952edee43ccd9
fishb6nes <fishb6nes@users.noreply.github.com>
2020-01-27 19:49:15 +0100

    Remove implicits from api layer

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/Implicits.scala
+++
@@ -1,7 +0,0 @@
-package gg.warcraft.monolith.api
-
-import gg.warcraft.monolith.api.core.event.EventService
-
-object Implicits {
-  implicit lazy val eventService: EventService = new EventService
-}
