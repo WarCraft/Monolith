9ee2d95141706128b4e98462b8ad9a907513799f
fishb6nes <fishb6nes@users.noreply.github.com>
2020-02-29 10:25:20 +0000

    Parse block and item type variants in codec layer

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/core/Codecs.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/core/Codecs.scala
@@ -2,6 +2,8 @@ package gg.warcraft.monolith.api.core
 
 import gg.warcraft.monolith.api.util.ColorCode
 import gg.warcraft.monolith.api.world.World
+import gg.warcraft.monolith.api.world.block.BlockTypeVariantOrState
+import gg.warcraft.monolith.api.world.item.ItemTypeOrVariant
 import io.circe.{Decoder, Encoder}
 import io.getquill.MappedEncoding
 
@@ -12,7 +14,17 @@ object Codecs {
     implicit val colorDec: Decoder[ColorCode] =
       Decoder.decodeString.emapTry(it => { Try(ColorCode.valueOf(it)) })
     implicit val colorEnc: Encoder[ColorCode] =
-      Encoder.encodeString.contramap[ColorCode](_.name)
+      Encoder.encodeString.contramap[ColorCode](_.toString)
+
+    implicit val blockDataDec: Decoder[BlockTypeVariantOrState] =
+      Decoder.decodeString.emapTry(it => { Try(worldService.parse(it)) })
+    implicit val blockDataEnc: Encoder[BlockTypeVariantOrState] =
+      Encoder.encodeString.contramap[BlockTypeVariantOrState](_.toString)
+
+    implicit val itemDataDec: Decoder[ItemTypeOrVariant] =
+      Decoder.decodeString.emapTry(it => { Try(itemService.parse(it)) })
+    implicit val itemDataEnc: Encoder[ItemTypeOrVariant] =
+      Encoder.encodeString.contramap[ItemTypeOrVariant](_.toString)
   }
 
   object Quill {
--- monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/ItemTypeOrVariant.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/world/item/ItemTypeOrVariant.scala
@@ -1,14 +1,3 @@
 package gg.warcraft.monolith.api.world.item
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import gg.warcraft.monolith.api.Implicits
-
-object ItemTypeOrVariant {
-  private val itemService = Implicits.itemService
-
-  @JsonCreator
-  def apply(data: String): ItemTypeOrVariant =
-    itemService.parseData(data)
-}
-
 trait ItemTypeOrVariant
