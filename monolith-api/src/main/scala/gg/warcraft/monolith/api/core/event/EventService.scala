055a2d7facf115652a0fc6da1a58e728cc49f248
fishb6nes <fishb6nes@users.noreply.github.com>
2020-03-11 11:50:09 +0000

    Merge event service wrapper into object

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/core/event/EventService.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/core/event/EventService.scala
@@ -1,5 +1,32 @@
 package gg.warcraft.monolith.api.core.event
 
+object EventService {
+  class Wrapper(eventService: EventService) extends EventService {
+    private var handlers: List[Event.Handler] = Nil
+
+    override def publish(event: Event): Unit =
+      eventService publish event
+
+    override def publish[T <: PreEvent](event: T): T =
+      eventService publish event
+
+    override def subscribe(handler: Event.Handler): Unit = {
+      handlers ::= handler
+      eventService subscribe handler
+    }
+
+    override def unsubscribe(handler: Event.Handler): Unit = {
+      handlers = handlers filter { _ != handler }
+      eventService unsubscribe handler
+    }
+
+    def unsubscribeAll(): Unit = {
+      handlers foreach eventService.unsubscribe
+      handlers = Nil
+    }
+  }
+}
+
 class EventService {
   private var handlers: List[Event.Handler] = Nil
 
