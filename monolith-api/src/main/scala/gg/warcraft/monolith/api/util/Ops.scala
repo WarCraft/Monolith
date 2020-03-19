108d0bd69c640f94923994eed46bff2070620415
fishb6nes <fishb6nes@users.noreply.github.com>
2020-03-19 18:26:55 +0000

    Add quality of life extension methods for piping and bringing futures back onto the server thread

---
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/util/Ops.scala
@@ -0,0 +1,24 @@
+package gg.warcraft.monolith.api.util
+
+import gg.warcraft.monolith.api.core.task.TaskService
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+
+object Ops {
+  @inline implicit final class ChainingOps[A](private val self: A) extends AnyVal {
+    def |>[B](f: A => B): B = f apply self
+  }
+
+  @inline implicit final class FutureOps[T](
+      private val self: Future[T]
+  ) extends AnyVal {
+    def immediatelyOrOnComplete[U](f: Try[T] => U)(
+        implicit context: ExecutionContext,
+        taskService: TaskService
+    ): Unit = self.value match {
+      case Some(result) => result |> f
+      case _            => self onComplete (it => taskService runNextTick (it |> f))
+    }
+  }
+}
