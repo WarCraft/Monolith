1daf7632057eb6490c349ad3ec595b2e9f642a34
fishb6nes <fishb6nes@users.noreply.github.com>
2020-04-09 21:47:23 +0100

    Check debugging state of principal

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/core/handler/DebuggingHandler.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/core/handler/DebuggingHandler.scala
@@ -3,15 +3,20 @@ package gg.warcraft.monolith.api.core.handler
 import gg.warcraft.monolith.api.core.auth.AuthService
 import gg.warcraft.monolith.api.core.event.Event
 import gg.warcraft.monolith.api.entity.{EntityInteractEvent, EntityService}
+import gg.warcraft.monolith.api.player.PlayerService
 
 class DebuggingHandler(
     authService: AuthService,
-    entityService: EntityService
+    entityService: EntityService,
+    playerService: PlayerService
 ) extends Event.Handler {
   override def handle(event: Event): Unit = event match {
-    case event: EntityInteractEvent =>
-      if (authService.isDebugging(event.playerId))
-        entityService.removeEntity(event.entityId)
+    case EntityInteractEvent(entityId, _, playerId, _, _, _) =>
+      playerService getPlayer playerId match {
+        case Some(player) =>
+          if (authService isDebugging player) entityService removeEntity entityId
+        case None =>
+      }
     case _ =>
   }
 }
