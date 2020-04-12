37acd8186db022c7ab29e1837e375e2ec6cbaf97
fishb6nes <fishb6nes@users.noreply.github.com>
2020-04-12 16:24:05 +0100

    Add onCommand stub to spigot monolith plugin

--- monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/SpigotMonolithPlugin.scala
+++ monolith-spigot/src/main/scala/gg/warcraft/monolith/spigot/SpigotMonolithPlugin.scala
@@ -7,6 +7,7 @@ import gg.warcraft.monolith.api.core.event.EventService
 import org.bukkit.event.{HandlerList, Listener}
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.Server
+import org.bukkit.command.{Command, CommandSender}
 import org.bukkit.plugin.Plugin
 
 abstract class SpigotMonolithPlugin extends JavaPlugin with MonolithPlugin {
@@ -23,6 +24,16 @@ abstract class SpigotMonolithPlugin extends JavaPlugin with MonolithPlugin {
     HandlerList unregisterAll this
     eventService.asInstanceOf[EventService.Wrapper].unsubscribeAll()
     databases foreach { _.close() }
+  }
+
+  override def onCommand(
+      sender: CommandSender,
+      command: Command,
+      label: String,
+      args: Array[String]
+  ): Boolean = {
+    // TODO call command service
+    false
   }
 
   protected def subscribe(listener: Listener): Unit =
