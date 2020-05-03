ca0449ea418afd2dade572875732aaaf80ed5cf0
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-03 17:32:27 +0100

    Add option to eval later inline or as closure

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/core/task/TaskService.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/core/task/TaskService.scala
@@ -4,8 +4,9 @@ import gg.warcraft.monolith.api.core.Duration
 import gg.warcraft.monolith.api.core.Duration._
 
 trait TaskService {
-  def evalLater(delay: Duration)(task: => Unit): Task
-  def runLater(delay: Duration, task: () => Unit): Task = evalLater(delay)(task())
+  def evalLater(delay: Duration, task: => Unit): Task
+  def evalLater(delay: Duration)(task: => Unit): Task = evalLater(delay)(task)
+  def runLater(delay: Duration, task: () => Unit): Task = evalLater(delay, task())
   def evalNextTick(task: => Unit): Task = evalLater(1.ticks)(task)
   def runNextTick(task: () => Unit): Task = evalLater(1.ticks)(task())
 
