84476333f4077e88c3f03caa5cc021120785e44b
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-17 11:19:28 +0100

    Rename data key column

--- monolith-spigot/src/main/resources/db/migration/V6__create_server_data_table.sql
+++ monolith-spigot/src/main/resources/db/migration/V6__create_server_data_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE server_data
 (
-    datum           TEXT        PRIMARY KEY,
+    data            TEXT        PRIMARY KEY,
     int_value       INTEGER     NOT NULL,
     float_value     REAL        NOT NULL,
     string_value    TEXT        NOT NULL
