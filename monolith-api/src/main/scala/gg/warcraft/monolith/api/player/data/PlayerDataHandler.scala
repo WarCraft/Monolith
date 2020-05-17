1e2762b2da6b895e0d6e9cadff67bbddfd0d234e
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-17 16:59:50 +0100

    Fix player data initialization

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/player/data/PlayerDataHandler.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/player/data/PlayerDataHandler.scala
@@ -1,26 +1,32 @@
 package gg.warcraft.monolith.api.player.data
 
-import gg.warcraft.monolith.api.core.event.Event
+import java.time.{Instant, ZoneOffset}
+import java.util.logging.Logger
+
+import gg.warcraft.monolith.api.core.event.{Event, PreEvent}
 import gg.warcraft.monolith.api.player.{PlayerDisconnectEvent, PlayerPreConnectEvent}
 
-class PlayerDataHandler(implicit service: PlayerDataService) extends Event.Handler {
+class PlayerDataHandler(implicit
+    logger: Logger,
+    service: PlayerDataService
+) extends Event.Handler {
   override def handle(event: Event): Unit = event match {
-    case it: PlayerPreConnectEvent => handlePreConnect(it)
-    case it: PlayerDisconnectEvent => handleDisconnect(it)
-    case _                         =>
+    case PlayerDisconnectEvent(player) =>
+      service.updatePlayerData(player.id)
+      service.invalidatePlayerData(player.id)
+    case _ =>
   }
 
-  private def handlePreConnect(event: PlayerPreConnectEvent): Unit = {
-    val now = System.currentTimeMillis()
-    val newData = service.loadPlayerData(event.playerId) match {
-      case Some(data) => data.copy(timeConnected = now, timeLastSeen = now)
-      case None       => PlayerData(event.playerId, None, now, now)
-    }
-    service setPlayerData newData
-  }
-
-  private def handleDisconnect(event: PlayerDisconnectEvent): Unit = {
-    service.updatePlayerData(event.player.id)
-    service.invalidatePlayerData(event.player.id)
+  override def reduce[T <: PreEvent](event: T): T = event match {
+    case PlayerPreConnectEvent(playerId, _, _, _) =>
+      val now = Instant.now().atOffset(ZoneOffset.UTC).toEpochSecond
+      // TODO convert values on PlayerData to LocalDateTime
+      val newData = service.loadPlayerData(playerId) match {
+        case Some(data) => data.copy(timeConnected = now, timeLastSeen = now)
+        case None       => PlayerData(playerId, None, now, now)
+      }
+      service.setPlayerData(newData)
+      event
+    case _ => event
   }
 }
--- monolith-api/src/main/scala/gg/warcraft/monolith/api/player/data/PlayerDataService.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/player/data/PlayerDataService.scala
@@ -13,8 +13,8 @@ import io.getquill.context.jdbc.JdbcContext
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.chaining._
 
-class PlayerDataService(
-    implicit logger: Logger,
+class PlayerDataService(implicit
+    logger: Logger,
     database: JdbcContext[SqliteDialect, SnakeCase],
     statisticService: StatisticService,
     teamService: TeamService
@@ -27,17 +27,15 @@ class PlayerDataService(
     Codecs.Quill.teamDecoder
   private implicit val teamEncoder: MappedEncoding[Team, String] =
     Codecs.Quill.teamEncoder
-  private implicit val dataInsertMeta: InsertMeta[PlayerData] =
-    insertMeta[PlayerData]()
 
   private var _data: Map[UUID, PlayerData] = Map.empty
 
-  def getPlayerData(id: UUID): Future[PlayerData] = {
-    _data get id match {
+  def getPlayerData(id: UUID): Future[PlayerData] =
+    _data.get(id) match {
       case Some(data) => data |> Future.successful
       case None =>
         Future {
-          run {
+          database.run {
             query[PlayerData] filter { _.id == lift(id) }
           }.headOption match {
             case Some(playerData) => playerData.asInstanceOf[PlayerData]
@@ -45,12 +43,11 @@ class PlayerDataService(
           }
         }
     }
-  }
 
   private[player] def setPlayerData(data: PlayerData): Unit = {
     _data += (data.id -> data)
     Future {
-      run {
+      database.run {
         query[PlayerData]
           .insert { lift(data) }
           .onConflictUpdate(_.id)(
@@ -60,23 +57,25 @@ class PlayerDataService(
           )
       }
     }
-    // TODO if team has changed fire event
+    // TODO if team has changed fire event, or move to playerservice?
   }
 
   private[data] def updatePlayerData(id: UUID): Unit = {
     val data = _data(id)
     val oldLastSeen = data.timeLastSeen
-    val newLastSeen = System.currentTimeMillis
+    val newLastSeen =
+      System.currentTimeMillis // TODO this needs fixing with LocalDateTime
 
-    statisticService increaseStatistic ("TimePlayed", newLastSeen - oldLastSeen, id)
-    this setPlayerData data.copy(timeLastSeen = newLastSeen)
+    statisticService.increaseStatistic("TimePlayed", newLastSeen - oldLastSeen, id)
+    setPlayerData(data.copy(timeLastSeen = newLastSeen))
   }
 
   private[data] def loadPlayerData(id: UUID): Option[PlayerData] =
-    run { query[PlayerData].filter { _.id == lift(id) } }.headOption
+    database.run { query[PlayerData].filter { _.id == lift(id) } }
+      .headOption
       .tap {
-        case Some(it) => _data += (it.id -> it)
-        case None     =>
+        case Some(data) => _data += (data.id -> data)
+        case None       =>
       }
 
   private[data] def invalidatePlayerData(id: UUID): Unit =
