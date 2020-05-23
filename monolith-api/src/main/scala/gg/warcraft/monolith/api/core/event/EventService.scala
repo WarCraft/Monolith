253c93fa3cf0f017eddda36fddac77b99d3643da
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-23 10:49:44 +0100

    Fix invalid forwarding of events to warpped event service

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/core/event/EventService.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/core/event/EventService.scala
@@ -2,10 +2,35 @@ package gg.warcraft.monolith.api.core.event
 
 import scala.util.chaining._
 
-object EventService {
-  class Wrapper(eventService: EventService) extends EventService {
-    private var handlers: List[Event.Handler] = Nil
+class EventService {
+  protected var handlers: List[Event.Handler] = Nil
 
+  def publish(event: Event): Unit = { // TODO add << method?
+    println(
+      s"PUBLISHING ${event.getClass.getSimpleName} to ${handlers.size} handlers"
+    )
+    handlers.foreach { _.handle(event) }
+  }
+
+  def publish[T <: PreEvent](event: T): T = {
+    println(s"REDUCING ${event.getClass.getSimpleName} to ${handlers.size} handlers")
+    handlers.foldLeft(event) { (event, handler) => handler.reduce(event) }.tap {
+      case it: CancellableEvent if it.allowed => handlers.foreach { _.handle(it) }
+      case it                                 => handlers.foreach { _.handle(it) }
+    }
+  }
+
+  def subscribe(handler: Event.Handler): Unit = {
+    println(s"SUBSCRIBING ${handler.getClass.getSimpleName} (${handlers.size})")
+    handlers ::= handler
+  }
+
+  def unsubscribe(handler: Event.Handler): Unit =
+    handlers = handlers.filter { _ != handler }
+}
+
+object EventService {
+  private[monolith] class Wrapper(eventService: EventService) extends EventService {
     override def publish(event: Event): Unit =
       eventService.publish(event)
 
@@ -28,24 +53,4 @@ object EventService {
       handlers = Nil
     }
   }
-}
-
-class EventService {
-  private var handlers: List[Event.Handler] = Nil
-
-  def publish(event: Event): Unit =
-    handlers.foreach { _.handle(event) }
-
-  def publish[T <: PreEvent](event: T): T =
-    handlers.foldLeft(event) { (event, handler) => handler.reduce(event) }
-      .tap {
-        case it: CancellableEvent if it.allowed => handlers.foreach { _.handle(it) }
-        case _                                  =>
-      }
-
-  def subscribe(handler: Event.Handler): Unit =
-    handlers ::= handler
-
-  def unsubscribe(handler: Event.Handler): Unit =
-    handlers = handlers.filter { _ != handler }
 }
