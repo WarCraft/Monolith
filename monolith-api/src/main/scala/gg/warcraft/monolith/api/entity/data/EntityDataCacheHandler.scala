56f4aa3001c0de334239b8ebea266d65eb16b987
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-25 19:28:31 +0100

    Initialize entity data for entities spawned through other plugins

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/entity/data/EntityDataCacheHandler.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/entity/data/EntityDataCacheHandler.scala
@@ -1,7 +1,7 @@
 package gg.warcraft.monolith.api.entity.data
 
-import gg.warcraft.monolith.api.core.event.Event
-import gg.warcraft.monolith.api.entity.EntityDeathEvent
+import gg.warcraft.monolith.api.core.event.{Event, PreEvent}
+import gg.warcraft.monolith.api.entity.{EntityDeathEvent, EntityPreSpawnEvent}
 
 class EntityDataCacheHandler(implicit
     service: EntityDataService
@@ -9,5 +9,12 @@ class EntityDataCacheHandler(implicit
   override def handle(event: Event): Unit = event match {
     case EntityDeathEvent(entity, _) => service.deleteEntityData(entity.id)
     case _                           =>
+  }
+
+  override def handle(event: PreEvent): Unit = event match {
+    case EntityPreSpawnEvent(entity, _, _, _) =>
+      if (!service.data.contains(entity.id))
+        service.setEntityData(EntityData(entity.id))
+    case _ =>
   }
 }
