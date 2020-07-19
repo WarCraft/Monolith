4205a146e24f656d36bb6d0049975f5d60ce19b0
fishb6nes <fishb6nes@users.noreply.github.com>
2020-07-19 14:23:06 +0100

    Remove auto start, should be called from implementing effect

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/effect/Effect.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/effect/Effect.scala
@@ -29,7 +29,7 @@ import gg.warcraft.monolith.api.core.Duration._
 import gg.warcraft.monolith.api.core.task.{Task, TaskService}
 import gg.warcraft.monolith.api.world.Location
 
-abstract class Effect(autoStart: Boolean = true)(implicit
+abstract class Effect(implicit
     taskService: TaskService
 ) extends Runnable
     with Playable {
@@ -38,8 +38,6 @@ abstract class Effect(autoStart: Boolean = true)(implicit
 
   private var renderers: List[EffectRenderer] = Nil
   private var task: Task = _
-
-  if (autoStart) start()
 
   def tick(): Unit = {}
 
