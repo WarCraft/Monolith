79181b4e52c7d96f1c12c2a71b8d122d54849076
fishb6nes <fishb6nes@users.noreply.github.com>
2020-07-23 20:18:31 +0100

    Add block backup restore command

---
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/block/backup/BlockBackupCommand.scala
@@ -0,0 +1,51 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 WarCraft <https://github.com/WarCraft>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package gg.warcraft.monolith.api.block.backup
+
+import gg.warcraft.monolith.api.core.auth.{AuthService, Principal}
+import gg.warcraft.monolith.api.core.command.Command
+import gg.warcraft.monolith.api.player.Player
+
+class BlockBackupCommand() extends Command("restore-all")
+
+object BlockBackupCommand {
+  class Handler(implicit
+      authService: AuthService,
+      blockBackupService: BlockBackupService
+  ) extends Command.Handler {
+    override def handle(
+        executor: Principal,
+        command: Command,
+        args: String*
+    ): Command.Result = executor match {
+      case player: Player =>
+        if (authService.isAdmin(player)) {
+          blockBackupService.restoreBackups()
+          Command.success
+        } else Command.invalid
+      case _ => Command.playersOnly
+    }
+  }
+}
