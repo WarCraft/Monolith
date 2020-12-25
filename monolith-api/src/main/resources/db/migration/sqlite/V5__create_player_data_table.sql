5bc37800cc74916e021fe1bde964f6f938db1cad
fishb6nes <fishb6nes@users.noreply.github.com>
2020-12-25 11:11:59 +0000

    Reduce variant size of sqlite datetimes

--- monolith-api/src/main/resources/db/migration/sqlite/V5__create_player_data_table.sql
+++ monolith-api/src/main/resources/db/migration/sqlite/V5__create_player_data_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE player_data
 (
     id             CHARACTER(32) PRIMARY KEY NOT NULL,
     team           VARCHAR(32)               NOT NULL,
-    time_connected VARCHAR(64)               NOT NULL,
-    time_last_seen VARCHAR(64)               NOT NULL
+    time_connected VARCHAR(32)               NOT NULL,
+    time_last_seen VARCHAR(32)               NOT NULL
 );
