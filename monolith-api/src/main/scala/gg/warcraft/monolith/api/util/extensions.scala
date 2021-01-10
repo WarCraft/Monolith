15af66b99189ea96bd3a825c917f63e8bc8ed33d
fishb6nes <fishb6nes@users.noreply.github.com>
2021-01-10 16:09:33 +0000

    Add capital letters to vowel check

--- monolith-api/src/main/scala/gg/warcraft/monolith/api/util/extensions.scala
+++ monolith-api/src/main/scala/gg/warcraft/monolith/api/util/extensions.scala
@@ -91,7 +91,7 @@ object option {
 
 object string {
   @inline implicit final class CharOps(private val self: Char) extends AnyVal {
-    def isVowel: Boolean = "aeiou".contains(self)
+    def isVowel: Boolean = "aeiouAEIOU".contains(self)
   }
 
   @inline implicit final class StringOps(private val self: String) extends AnyVal {
